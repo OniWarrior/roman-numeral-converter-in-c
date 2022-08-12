263af4391735e0b44b36905539853bfb906c0460
Stephen Aranda <aranda.stephen88@gmail.com>
2022-08-12 18:45:04 -0500

    created the ifndef block

--- roman-numeral-converter-protos.h
+++ roman-numeral-converter-protos.h
@@ -4,3 +4,8 @@
 ** Description: This file defines all my prototypes I intend to use in the roman numeral app.
 ** Date: 8/12/2022
 */
+
+#ifndef ROMAN_NUMERAL_PROTOS
+#define ROMAN_NUMERAL_PROTOS
+
+#endif
