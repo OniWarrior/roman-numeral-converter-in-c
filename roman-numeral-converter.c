e1b4a5fbad48d7d6c78253a0b5618dce13608a29
Stephen Aranda <aranda.stephen88@gmail.com>
2022-08-12 20:11:10 -0500

    added author comment

---
+++ roman-numeral-converter.c
@@ -0,0 +1,8 @@
+/*
+ * author: stephen aranda
+ * file  : roman-numeral-converter.c
+ * date  : 8/12/2022
+ * description: This is the entry point for the roman numeral converter app 
+ * written in C language.
+ */
+
